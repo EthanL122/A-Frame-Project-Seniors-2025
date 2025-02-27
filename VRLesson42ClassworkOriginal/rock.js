848c476ab7cfd49a800e0dd89dc77706720dd57b
EthanL122 <EthanL122@nycstudents.net>
2025-02-26 23:13:24 -0500

    Update rock.js

--- VRLesson42ClassworkOriginal/rock.js
+++ VRLesson42ClassworkOriginal/rock.js
@@ -1,6 +1,5 @@
 class Rock {
-  constructor(x, y, z, isUp, color) {
-    // Create the rock element
+  constructor(x, y, z, color) {
     let rock = document.createElement("a-entity");
     rock.setAttribute("geometry", "primitive: dodecahedron");
     rock.setAttribute("position", `${x} ${y} ${z}`);
@@ -11,20 +10,25 @@ class Rock {
     rock.setAttribute("rotation", `${a} ${a} ${a}`);
     
     // Track if the rock is flipped
-    let isFlipped = isUp;
+    let isUp = false;
     
     // Event listener for flipping and moving the rock
     rock.addEventListener("click", function () {
       let currentRotation = rock.getAttribute("rotation");
       let currentPosition = rock.getAttribute("position");
       
-      if (!isFlipped) {
+      // Check if the rock's color is white and show the alert
+      if (color === "white") {
+        alert("Boom, you died!");
+      }
+      
+      if (!isUp) {
         rock.setAttribute("animation__flip", `property: rotation; to: ${currentRotation.x + 60} ${currentRotation.y + 50} ${currentRotation.z + 60}; dur: 1000; easing: easeInOutQuad`);
         rock.setAttribute("animation__move", `property: position; to: ${x} ${y + 1} ${z}; dur: 1000; easing: easeInOutQuad`);
       } else {
         rock.setAttribute("animation__move", `property: position; to: ${x} ${y} ${z}; dur: 1000; easing: easeInOutQuad`);
       }
-      isFlipped = !isFlipped;
+      isUp = !isUp;
     });
 
     return rock;
